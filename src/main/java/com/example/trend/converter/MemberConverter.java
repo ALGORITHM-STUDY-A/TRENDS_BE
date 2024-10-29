338494795b5cf08142c4033336788f44ecb64d92
전재연 <sunrox1107@gmail.com>
2024-10-29 10:49:24 +0900

    [feat] 회원가입 서비스 코드 완성

--- src/main/java/com/example/trend/converter/MemberConverter.java
+++ src/main/java/com/example/trend/converter/MemberConverter.java
@@ -1,4 +1,12 @@
 package com.example.trend.converter;
 
+import com.example.trend.domain.Member;
+import com.example.trend.repository.MemberRepository;
+import com.example.trend.web.dto.MemberJoinDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@RequiredArgsConstructor
 public class MemberConverter {
+
 }
