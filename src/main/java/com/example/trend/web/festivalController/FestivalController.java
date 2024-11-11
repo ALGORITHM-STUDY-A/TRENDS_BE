891b91e7f1f9a23b71b5d5b7d76611f03b6d6aaf
gdbs1107 <gdbs1107@naver.com>
2024-11-11 11:06:55 +0900

    [feat] 어노테이션 추가

--- src/main/java/com/example/trend/web/festivalController/FestivalController.java
+++ src/main/java/com/example/trend/web/festivalController/FestivalController.java
@@ -1,4 +1,12 @@
 package com.example.trend.web.festivalController;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/festival")
+@RequiredArgsConstructor
 public class FestivalController {
+
 }
