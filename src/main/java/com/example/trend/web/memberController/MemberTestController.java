6ea73a39544d80bbb8492e336bfa57f69998202d
gdbs1107 <gdbs1107@naver.com>
2024-11-11 15:33:40 +0900

    [feat] memberTestController 생성

---
+++ src/main/java/com/example/trend/web/memberController/MemberTestController.java
@@ -0,0 +1,14 @@
+package com.example.trend.web.memberController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MemberTestController {
+
+    @GetMapping("/register")
+    public String registerPage() {
+        return "register"; // templates/register.html 파일을 렌더링
+    }
+
+}
