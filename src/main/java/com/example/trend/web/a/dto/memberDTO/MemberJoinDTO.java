0a135809b587b7684622c5d37b046f7c66f0179a
gdbs1107 <gdbs1107@naver.com>
2024-11-11 15:32:09 +0900

    [feat] 주소 저장을 위해 도로명 주소 api 테스트를 위해 thyemleaf 의존성 주입

--- src/main/java/com/example/trend/web/a/dto/memberDTO/MemberJoinDTO.java
+++ src/main/java/com/example/trend/web/a/dto/memberDTO/MemberJoinDTO.java
@@ -9,23 +9,24 @@ public class MemberJoinDTO {
     @Getter
     public static class MemberJoinRequestDTO{
 
+
+        @Schema(description = "회원이름 입니다")
+        String name;
+
+        @Schema(description = "회원전화번호 입니다")
+        String phoneNumber;
+        
+        @Schema(description = "회원이메일 입니다")
+        String email;
+
         @Schema(description = "ID입니다 <br> 6~20자의 영문,숫자")
         String username;
 
         @Schema(description = "비밀번호입니다.<br>8~12문자의 영문, 숫자, 특수문자를 포함해야 합니다.")
         String password;
 
-        @Schema(description = "회원이름 입니다")
-        String name;
-
         @Schema(description = "회원 닉네임 입니다")
         String nickname;
-
-        @Schema(description = "회원전화번호 입니다")
-        String phoneNumber;
-
-        @Schema(description = "회원이메일 입니다")
-        String email;
 
 
         // 주소를 한 줄로 받는데 어떻게 이걸 구분을 해야할까
