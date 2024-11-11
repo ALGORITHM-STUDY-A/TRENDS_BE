3bae4ba7fd0b1128d7e7b4d6a59f8f8309463828
gdbs1107 <gdbs1107@naver.com>
2024-11-11 18:53:19 +0900

    [feat] 기업 username 중복 검사 메서드 구현

--- src/main/java/com/example/trend/repository/CompanyRepository.java
+++ src/main/java/com/example/trend/repository/CompanyRepository.java
@@ -1,21 +1,12 @@
 package com.example.trend.repository;
 
 import com.example.trend.domain.Company;
-import com.example.trend.domain.Member;
-import com.example.trend.domain.enumClass.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Optional;
 
 public interface CompanyRepository extends JpaRepository<Company, Long> {
     Optional<Company> findByUsername(String username);
 
     Boolean existsByUsername(String username);
-
-    @Query("SELECT c FROM Company c WHERE c.status = :status AND c.inactiveDate <= :cutoffDate")
-    List<Company> findInactiveMembersForDeletion(@Param("status") Status status, @Param("cutoffDate") LocalDateTime cutoffDate);
 }
