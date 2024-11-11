388aebf6ad019e8cd645c5dae1dcc268a0d3f674
gdbs1107 <gdbs1107@naver.com>
2024-11-11 15:35:08 +0900

    [feat] daum 주소 api를 도로명 주소를 넣는 회원가입 api 구현

--- src/main/java/com/example/trend/web/memberController/MemberTestController.java
+++ src/main/java/com/example/trend/web/memberController/MemberTestController.java
@@ -8,7 +8,7 @@ public class MemberTestController {
 
     @GetMapping("/register")
     public String registerPage() {
-        return "register"; // templates/register.html 파일을 렌더링
+        return "register.html"; // templates/register.html.html 파일을 렌더링
     }
 
 }
