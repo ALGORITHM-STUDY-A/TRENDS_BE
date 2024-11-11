2bbca2e384a997837df617a599f6f261431c5248
gdbs1107 <gdbs1107@naver.com>
2024-11-11 18:27:57 +0900

    [feat] username 중복 검사 메서드

--- src/main/java/com/example/trend/repository/MemberRepository.java
+++ src/main/java/com/example/trend/repository/MemberRepository.java
@@ -2,9 +2,12 @@ package com.example.trend.repository;
 
 import com.example.trend.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
 }
