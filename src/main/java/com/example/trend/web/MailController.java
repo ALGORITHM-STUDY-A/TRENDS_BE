ebd827f40d3076420005f9dc407a65e0f5781392
gdbs1107 <gdbs1107@naver.com>
2024-11-14 13:17:09 +0900

    [feat] email API 명세 작성

--- src/main/java/com/example/trend/web/MailController.java
+++ src/main/java/com/example/trend/web/MailController.java
@@ -1,6 +1,8 @@
 package com.example.trend.web;
 
 import com.example.trend.service.MailService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,6 +14,7 @@ import java.util.HashMap;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/mails")
+@Tag(name = "이메일 인증을 위한 API")
 public class MailController {
 
     private static final Logger log = LoggerFactory.getLogger(MailController.class);
@@ -19,6 +22,7 @@ public class MailController {
     private int number; // 이메일 인증 숫자를 저장하는 변수
 
     // 인증 이메일 전송
+    @Operation(summary = "인증 이메일 전송 API")
     @PostMapping("/mailSend")
     public HashMap<String, Object> mailSend(@RequestParam String mail) {
         HashMap<String, Object> map = new HashMap<>();
@@ -41,6 +45,7 @@ public class MailController {
 
 
     // 인증번호 일치여부 확인
+    @Operation(summary = "인증번호 일치여부 확인 API")
     @GetMapping("/mailCheck")
     public ResponseEntity<?> mailCheck(@RequestParam String userNumber) {
 
