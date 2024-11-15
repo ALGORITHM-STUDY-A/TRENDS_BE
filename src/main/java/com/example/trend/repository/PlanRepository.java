a5bd6ce420c89c74c847955ff25ed2272cc23176
gdbs1107 <gdbs1107@naver.com>
2024-11-15 13:31:19 +0900

    [feat] 배너 조회 API fetch join 구현

--- src/main/java/com/example/trend/repository/PlanRepository.java
+++ src/main/java/com/example/trend/repository/PlanRepository.java
@@ -1,11 +1,13 @@
 package com.example.trend.repository;
 
 import com.example.trend.domain.Plan;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface PlanRepository extends JpaRepository<Plan, Long> {
 
+    @EntityGraph(attributePaths = {"member"})
     List<Plan> findTop4ByOrderByLikesCountDesc();
 }
